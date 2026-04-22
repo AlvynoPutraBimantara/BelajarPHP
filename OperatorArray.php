6694904543fe68af7a079bc90e1fea0860dc2f5b
Alvyno Putra Bimantara <alvyno.putra.b@gmail.com>
2026-04-22 19:01:18 +0700

    OperatorArray

--- OperatorArray.php
+++ OperatorArray.php
@@ -5,16 +5,15 @@ $Middle = ["NamaTengah" => "Putra"];
 $Last = ["NamaBelakang" => "Bimantara"];
 
 $full = $first + $Middle + $Last;
+// var_dump($full);
+// echo "\n";
+// echo "Nama : " . $Middle ["NamaTengah"];
 
-var_dump($full);
-echo "\n";
-echo "Nama : " . $first ["NamaDepan"];
-
-//echo "Nama : " . $full["NamaDepan"] . " ". $Middle["NamaTengah"] . " " . $full["NamaBelakang"];
+// echo "Nama : " . $full["NamaDepan"] . " ". $full["NamaTengah"] . " " . $full["NamaBelakang"];
 
 
 //echo "Nama : " . $full ;
-echo "\n";
+// echo "\n";
 
 $fullName = ["NamaDepan" => "Antiseptik", "NamaTengah" => "Alkohol", "NamaBelakang" => "Aethanolum" ];
 
@@ -23,10 +22,10 @@ echo "Nama : " . $fullName ["NamaDepan"] . " " . $fullName["NamaTengah"] . " " .
 
 echo "\n";
 
-$arrayA = [5, 4, 3, 2, 2];
-$arrayB = [1, 2, 3, 4, 5];
+$arrayA = [6, 5, 4, 3, 2, 1];
+$arrayB = [1, 2, 3, 4, 5, 6 ];
 
-$arrayC = ($arrayA + $arrayB);
+$arrayC = array_merge($arrayB, $arrayA);
 var_dump($arrayC);
 print_r ($arrayC);
 print_r ($fullName);
